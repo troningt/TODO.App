37c830fbb07feba8e2dbe26977872d2dbce6d0c4
Roning <roning1.0@gmail.com>
2022-03-14 14:26:08 -0500

    Create EurekaServer for dockerized

---
+++ DockerNetflix-EurekaServer/src/test/java/com/troningt/todolist/docker/spring/cloud/netflix/DockerNetflixEurekaServer/DockerNetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troningt.todolist.docker.spring.cloud.netflix.DockerNetflixEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerNetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
